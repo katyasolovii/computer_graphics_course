510bf639305ef3dfa6c47b5140a72d83e93593d8
katyasolovii <soloviyk@gmail.com>
2026-04-11 22:37:58 +0300

    added hw_2_2

--- OpenGL_learning/src/main.cpp
+++ OpenGL_learning/src/main.cpp
@@ -1,16 +1,80 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 #include <iostream>
+#include "shader_utils.h"
 
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 int main() {
-    // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the <b>lang</b> variable name to see how CLion can help you rename it.
-    auto lang = "C++";
-    std::cout << "Hello and welcome to " << lang << "!\n";
+    if (!glfwInit()) return -1;
 
-    for (int i = 1; i <= 5; i++) {
-        // TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        std::cout << "i = " << i << std::endl;
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL Rectangle Interpolation", NULL, NULL);
+    if (!window) {
+        glfwTerminate();
+        return -1;
     }
 
+    glfwMakeContextCurrent(window);
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) return -1;
+
+    // Завантаження шейдерів
+    std::string vShader = "res/shaders/triangle.vert";
+    std::string fShader = "res/shaders/triangle.frag";
+    GLuint shaderProgram = createProgram(vShader, fShader);
+
+    // Координати вершин прямокутника (X, Y, Z)
+    float vertices[] = {
+        -0.5f,  0.5f, 0.0f,  // Вершина 1 (Верхній лівий кут)
+         0.5f,  0.5f, 0.0f,  // Вершина 2 (Верхній правий кут)
+        -0.5f, -0.5f, 0.0f,  // Вершина 3 (Нижній лівий кут)
+         0.5f, -0.5f, 0.0f   // Вершина 4 (Нижній правий кут)
+    };
+
+    // Кольори для кожної вершини (R, G, B)
+    float colors[] = {
+        1.0f, 0.0f, 0.0f,  // Червоний (Вершина 1)
+        1.0f, 1.0f, 0.0f,  // Жовтий (Вершина 2)
+        0.0f, 1.0f, 0.0f,  // Зелений (Вершина 3)
+        0.0f, 0.0f, 1.0f   // Синій (Вершина 4)
+    };
+
+    GLuint VAO, VBO_pos, VBO_color;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO_pos);
+    glGenBuffers(1, &VBO_color);
+
+    glBindVertexArray(VAO);
+
+    // Буфер позицій
+    glBindBuffer(GL_ARRAY_BUFFER, VBO_pos);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    // Буфер кольорів
+    glBindBuffer(GL_ARRAY_BUFFER, VBO_color);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(colors), colors, GL_STATIC_DRAW);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(1);
+
+    while (!glfwWindowShouldClose(window)) {
+        glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        
+        glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO_pos);
+    glDeleteBuffers(1, &VBO_color);
+    glfwTerminate();
     return 0;
-    // TIP See CLion help at <a href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>. Also, you can try interactive lessons for CLion by selecting 'Help | Learn IDE Features' from the main menu.
 }
